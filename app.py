c21c73b76f5bf13cd79599c34df69915633cbd53
RauziDssh <rauzi.univ@gmail.com>
2015-11-08 22:55:07 +0900

    initialcommit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-#coding utf-8
+﻿# -*- coding: utf-8 -*-
 from bottle import route,run
 from requests_oauthlib import OAuth1Session
 import conf
 import json
+import os
 import re
 
 
@@ -38,26 +39,43 @@ def getOC(campus):
          "screen_name":name,
          "count" : "10"
     }
+    oc = ""
+    stat_change_time = ""
+        
     req = twitter.get(url,params = params)
     if req.status_code == 200:
         timeline = json.loads(req.text)
-        for tweet in timeline:
+        for tweet in timeline:           
+            
+            #statusの更新
             tweet_text = tweet["text"]
-            if re.search("OPEN",tweet_text):
-                return "OPEN"
-            if re.search("CLOSE",tweet_text):
-                return "CLOSE"
+            m = re.search("OPEN|CLOSE",tweet_text)
+            if m is not None:
+                stat = {
+                    "status":tweet_text[m.start():m.end()],
+                    "stat_change_time":getTime(tweet_text),
+                    "campus":"imadegawa" if campus == "i" else "kyotanabe"
+                }
+                return stat
             return "ERROR"
     else:
         return "ERROR"
 
+def getTime(tweet_text):
+    m = re.search(r"\d*時\d*分",tweet_text)
+    if m is not None:
+        return tweet_text[m.start():m.end()]
+    else:
+        return "GET TIME ERROR"
+
 @route('/')
 def denden():
-    return "でんでん～"
+    return "でんでんでんくす"
 
 @route('/box&c=<campus>',method = 'GET')
 def oc_show(campus = 'k'):
     status = getOC(campus)
-    return {"status":status}
+    return json.dumps(status,ensure_ascii=False)
 
-run(host="0.0.0.0", port=8124)
+run(host="0.0.0.0",port=int(os.environ.get("PORT", 5000)))
+#run(host="localhost",port=int(os.environ.get("PORT", 5000)))
