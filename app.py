8069aed450d2b683b85a9e052b152a8fabc36745
RauziDssh <bmn1070@mail4.doshisha.ac.jp>
2015-11-12 15:45:34 +0900

    O/C取得部分の改善

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if req.status_code == 200:
         print(tweet["text"])
 else:
     print("Error:%d"%req.status_code)
-
+"""
 def getOC(campus):
     
     name = "denx_robotaro"
@@ -60,7 +60,71 @@ def getOC(campus):
             return "ERROR"
     else:
         return "ERROR"
+"""
 
+def getReq(campus):
+    name = "denx_robotaro"
+    if campus == "i":
+        name = "denx_imadegawa"
+    params = {
+         "screen_name":name,
+         "count" : "10"
+    }   
+    req = twitter.get(url,params = params)
+    if req.status_code == 200:
+        timeline = json.loads(req.text)
+        c = "imadegawa" if campus == "i" else "kyotanabe"
+        return getOCstatus(c,timeline)
+    else:
+        return req.status_code
+    
+
+def getOCstatus(campus,timeline):
+    r_timeline = []
+    for tweet in timeline:
+        r_timeline.insert(0,tweet)
+    oc = ""
+    stat_change_time = ""
+    if campus == "kyotanabe":
+        oc_233 = "CLOSE"
+        oc_234 = "CLOSE"
+        for tweet in r_timeline:
+            tweet_text = tweet["text"]
+            
+            #print(tweet_text)
+            
+            m = re.search(r"OPEN|CLOSE",tweet_text)
+            r = re.search(r"\[234\]|\[233\]",tweet_text)
+            #print(m.group())
+            #print(r.group())
+            if r.group() == "[233]":
+                oc_233 = m.group()
+            if r.group() == "[234]":
+                oc_234 = m.group()
+            stat_change_time = getTime(tweet_text)
+            
+        #print(oc_233)
+        #print(oc_234)
+        if oc_233 == "OPEN" or oc_234 == "OPEN":
+           oc = "OPEN"
+        if oc_233 == "CLOSE" and oc_234 == "CLOSE":
+           oc = "CLOSE"
+    if campus == "imadegawa":
+        for tweet in r_timeline:
+            tweet_text = tweet["text"]
+            m = re.search(r"OPEN|CLOSE",tweet_text)
+            if m is not None:
+                oc = m.group()
+            else:
+                oc = "ERROR"
+            stat_change_time = getTime(tweet_text)
+    stat = {
+                    "status":oc,
+                    "stat_change_time":stat_change_time,
+                    "campus":campus
+    }
+    return stat
+          
 def getTime(tweet_text):
     m = re.search(r"\d*時\d*分",tweet_text)
     if m is not None:
@@ -74,7 +138,8 @@ def denden():
 
 @route('/box&c=<campus>',method = 'GET')
 def oc_show(campus = 'k'):
-    status = getOC(campus)
+    #status = getOC(campus)
+    status = getReq(campus)
     return json.dumps(status,ensure_ascii=False)
 
 run(host="0.0.0.0",port=int(os.environ.get("PORT", 5000)))
